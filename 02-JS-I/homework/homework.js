8ff794d6ed96e7044952b65ec776a3f2eb41089d
thiki11 <thikiledesma@gmail.com>
2022-09-26 20:43:42 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = ngit ull;
+const nuevoNum = 1;
 
 // Crea una variable booleana:
 const nuevoBool = null;
